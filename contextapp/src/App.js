598803b020e0d525158b79f857ae6e2992c4a9c6
vermouth <vermouth_fee@outlook.com>
2019-08-12 10:47:55 +0800

    add theme toggle

--- contextapp/src/App.js
+++ contextapp/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import BookList from './components/BookList';
+import Navbar from './components/Navbar';
+import ThemeContextProvider from './contexts/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
 
 function App() {
   return (
-   <div></div>
+    <div className="App">
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+        <ThemeToggle />
+      </ThemeContextProvider>
+    </div>
   );
 }
 
